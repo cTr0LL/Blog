3e2fdac9d6726b4a7c6ac48b41ffb793604b269d
cTr0LL <ysli@uwaterloo.ca>
2023-10-10 17:03:50 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import '../styles/globals.scss';
+import 'tailwindcss/tailwind.css';
+import React from 'react';
+import {Layout} from '../components';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
